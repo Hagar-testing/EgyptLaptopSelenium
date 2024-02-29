ba0345e54cc3a830b787834a32e1aaf90b4508ac
Hagar <hagar.testing@gmail.com>
2024-02-29 11:28:37 +0200

    Add RetryAnalyzer and TestngListener

---
+++ src/test/java/com/egyptlaptop/listeners/RetryAnalyzer.java
@@ -0,0 +1,29 @@
+package com.egyptlaptop.listeners;
+
+import org.testng.IRetryAnalyzer;
+import org.testng.ITestResult;
+
+public class RetryAnalyzer implements IRetryAnalyzer {
+    private int retryCount = 0;
+
+    @Override
+    public boolean retry(ITestResult result) {
+        int maxRetryCount = 1;
+        if (retryCount < maxRetryCount) {
+            System.out.println("Retrying test " + result.getName() + " with status " +
+                    getResultStatusName(result.getStatus()) + " for the " + (retryCount + 1) + " time(s).");
+            retryCount++;
+            return true;
+        }
+        return false;
+    }
+
+    private String getResultStatusName(int status) {
+        return switch (status) {
+            case ITestResult.SUCCESS -> "SUCCESS";
+            case ITestResult.FAILURE -> "FAILURE";
+            case ITestResult.SKIP -> "SKIP";
+            default -> "UNKNOWN";
+        };
+    }
+}
