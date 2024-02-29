009f6dda1676016ffa6fe572fa39c7430a946ce3
Hagar <hagar.testing@gmail.com>
2024-02-29 10:43:44 +0200

    Add ConfigConstants

---
+++ src/test/java/com/egyptlaptop/constants/ConfigConstants.java
@@ -0,0 +1,14 @@
+package com.egyptlaptop.constants;
+
+public class ConfigConstants {
+
+    public static final String BASE_URL = "baseUrl";
+    public static final String ENV = "env";
+    public static final String PRODUCTION = "production";
+    public static final String STAGING = "staging";
+    public static final String CONFIG_PATH =  "src/test/java/com/egyptlaptop/config/";
+    public static final String PRODUCTION_PROPERTIES_PATH = CONFIG_PATH + "production.properties";
+    public static final String STAGING_PROPERTIES_PATH =  CONFIG_PATH + "prod.properties";
+
+
+}
