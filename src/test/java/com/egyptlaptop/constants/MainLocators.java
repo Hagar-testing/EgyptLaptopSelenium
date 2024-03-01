679bd259653987c89a56822e3d874f0dfe60f330
Hagar <hagar.testing@gmail.com>
2024-03-01 09:54:47 +0200

    Creat Main page and add its methods

---
+++ src/test/java/com/egyptlaptop/constants/MainLocators.java
@@ -0,0 +1,8 @@
+package com.egyptlaptop.constants;
+
+public class MainLocators {
+
+    public static final String SEARCH_INPUT_ID = "search_input";
+
+    public static final String SEARCH_PRODUCTS_LIST_TITLES_XPATH = "//ul[@class='snize-ac-results-multicolumn-list']//span[@class='snize-title']";
+}
---
+++ src/test/java/com/egyptlaptop/pages/MainPage.java
@@ -0,0 +1,49 @@
+package com.egyptlaptop.pages;
+
+import com.egyptlaptop.base.BasePage;
+import com.egyptlaptop.utils.ConfigUtils;
+import io.qameta.allure.Step;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.List;
+
+import static com.egyptlaptop.constants.JsonKeysConstants.CATEGORY_XPATH;
+import static com.egyptlaptop.constants.MainLocators.SEARCH_INPUT_ID;
+import static com.egyptlaptop.constants.MainLocators.SEARCH_PRODUCTS_LIST_TITLES_XPATH;
+
+public class MainPage extends BasePage {
+
+    @FindBy(id = SEARCH_INPUT_ID)
+    WebElement search_input;
+
+    @FindBy(xpath = SEARCH_PRODUCTS_LIST_TITLES_XPATH)
+    List<WebElement> searchResults_list;
+
+    @FindBy(xpath = CATEGORY_XPATH)
+    WebElement first_category_div;
+    public MainPage(WebDriver driver) {
+        super(driver);
+    }
+
+    @Step
+    public MainPage load(){
+        driver.get(ConfigUtils.getBaseUrl() );
+        return this;
+    }
+    @Step
+    public MainPage sendSearchInput(String searchText) {
+        elementInteraction.setInput(search_input,searchText);
+        return this;
+    }
+
+    public boolean isSearchResultNotEmpty(){
+        return  searchResults_list.size() > 0;
+    }
+
+    public MainPage clickOnCategory(){
+         elementInteraction.simpleClick(first_category_div);
+         return this;
+    }
+}
