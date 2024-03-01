84201fc1ae4e3b7b4ba76176723351ad72283167
Hagar <hagar.testing@gmail.com>
2024-03-01 09:56:25 +0200

    Create JsonKeysConstants

---
+++ src/test/java/com/egyptlaptop/constants/JsonKeysConstants.java
@@ -0,0 +1,6 @@
+package com.egyptlaptop.constants;
+
+public class JsonKeysConstants {
+    public static final String SEARCH_TEXT = "search_text";
+    public static final String CATEGORY_XPATH = "//div[@class='span3 banners-grid-resizable '][1]/div[1]";
+}
