4b19afff71a703576e91096d568d7f79763f652d
Hagar <hagar.testing@gmail.com>
2024-03-01 14:27:18 +0200

    Create empty cart page

---
+++ src/test/java/com/egyptlaptop/pages/CartPage.java
@@ -0,0 +1,10 @@
+package com.egyptlaptop.pages;
+
+import com.egyptlaptop.base.BasePage;
+import org.openqa.selenium.WebDriver;
+
+public class CartPage extends BasePage {
+    public CartPage(WebDriver driver) {
+        super(driver);
+    }
+}
