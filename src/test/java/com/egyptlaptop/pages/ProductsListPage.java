6f7fc381dbef6c9835a0edae404c517ec6672e30
Hagar <hagar.testing@gmail.com>
2024-03-01 14:28:25 +0200

    Create ProductsListPage

---
+++ src/test/java/com/egyptlaptop/pages/ProductsListPage.java
@@ -0,0 +1,30 @@
+package com.egyptlaptop.pages;
+
+import com.egyptlaptop.base.BasePage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class ProductsListPage extends BasePage {
+
+    @FindBy(xpath = "//div[@id='categories_view_pagination_contents']//div[@class='ty-column4']//div[@class='ut2-gl__item ']")
+    WebElement product;
+
+
+    @FindBy(xpath = "//li[contains(., 'In stock')]/input[@type='checkbox']")
+    WebElement inStock_checkbox;
+    public ProductsListPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public ProductDetailsPage clickOnProduct(){
+        elementInteraction.simpleClick(product);
+        return new ProductDetailsPage(driver);
+    }
+
+    public ProductsListPage checkInStockCheckBox(){
+        inStock_checkbox.click();
+        return this;
+    }
+
+}
