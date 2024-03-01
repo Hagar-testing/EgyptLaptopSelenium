ab2b600a845de2a2f35b7424760a93b0f32f4013
Hagar <hagar.testing@gmail.com>
2024-03-01 19:11:27 +0200

    Handle redirecting to chaeckout from cart

--- src/test/java/com/egyptlaptop/pages/CartPage.java
+++ src/test/java/com/egyptlaptop/pages/CartPage.java
@@ -4,18 +4,25 @@ import com.egyptlaptop.base.BasePage;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import static com.egyptlaptop.constants.CartLocators.CHECKOUT_BTN;
 
 public class CartPage extends BasePage {
 
-
     @FindBy(xpath = "//div[@class=\"ty-center ty-value-changer cm-value-changer\"]//input")
     WebElement quantity_input;
+
+    @FindBy(xpath = "//a[@class=\"ty-btn ty-btn__primary \"]")
+    WebElement checkout_button;
     public CartPage(WebDriver driver) {
         super(driver);
     }
 
     public String getProductQuantityInCart(){
          return quantity_input.getAttribute("value");
+    }
 
+    public CheckoutPage clickOnCheckout(){
+        elementInteraction.javascriptClick(checkout_button);
+        return new CheckoutPage(driver);
     }
 }
